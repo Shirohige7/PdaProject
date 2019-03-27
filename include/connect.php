1ddc157755f44484651413145320b50d385354f4
Shirohige7 <47906642+Shirohige7@users.noreply.github.com>
2019-03-27 23:11:54 +0200

    Added a database connection file

---
+++ include/connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "manos";
+$dbPassword = "lin3age73591";
+$dbName = "projectpda";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+?>
